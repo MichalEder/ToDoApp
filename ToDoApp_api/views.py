3410dfdb1cdafbdd1ca3156b55c7174b40331089
MichalEder <edermichal.eder@gmail.com>
2024-02-03 21:24:37 +0100

    Test APIVIew added

--- ToDoApp_api/views.py
+++ ToDoApp_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+    """TEst APIView"""
+    def get(self, request, format=None):
+        """Returns list of APIView features"""
+        an_api_view = [
+            'Uses HTTP methods as function',
+            'IS similat to DjangoView',
+            'Gives you the most control over aplication logic',
+        ]
+
+        return Response({'message': 'Hello', 'an_api_view': an_api_view})
+        # Create your views here.
