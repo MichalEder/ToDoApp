f84a27efdd645ed5206366a8396f0e44276cd6e8
MichalEder <edermichal.eder@gmail.com>
2024-02-04 22:23:41 +0100

    configured for deployment

--- ToDoApp_project/settings.py
+++ ToDoApp_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ko!g0pqgoav30ow=s3grmt-v9$tp10ij--2-)g7l6(_qx1%*j*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'ToDoApp_api.UserProfile'
+
+STATIC_ROOT = 'static/'
