18ae195e8252b1e670866e99cf7f45fd66eefe8c
MichalEder <edermichal.eder@gmail.com>
2024-02-06 15:39:18 +0100

    Code with no use deleted

--- ToDoApp_api/urls.py
+++ ToDoApp_api/urls.py
@@ -3,13 +3,11 @@ from rest_framework.routers import DefaultRouter
 from ToDoApp_api import views
 
 router = DefaultRouter()
-router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
-router.register('profile', views.UserProfileViewSet)
-router.register('task', views.TaskViewSet)
+router.register('profile', views.UserProfileViewSet, basename='profile')
+router.register('task', views.TaskViewSet, basename='task')
 
 
 urlpatterns = [
-    path('hello-view/', views.HelloApiView.as_view()),
     path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls))
 ]
